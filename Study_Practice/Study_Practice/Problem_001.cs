06ab5b7dc0ea36ddd99baaab1ed3d03c27435c17
Vinh <lgv.vinh@gmail.com>
2023-04-08 19:03:53 +0700

    Upload problem 001

---
+++ Study_Practice/Study_Practice/Problem_001.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Study_Practice;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Bài 1: Tính S(n) = 1 + 2 + 3 + … + n
+        // Tao input
+        Console.WriteLine("Nhap so n:");
+        int n = int.Parse(Console.ReadLine());
+
+        // Tao bien
+        int S = 0;
+
+        // Loop
+        for (int i = 1; i <= n; i++)
+        {
+            S += i;
+        }
+        Console.WriteLine($"S({n}) = " + S);
+    }
+}
+
