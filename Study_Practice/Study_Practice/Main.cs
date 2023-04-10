4479a8e64c5c65eb4e0550dc10e12df68a8b2a7d
Lac Gia Vinh <vinh@Vinhs-MacBook-Air.local>
2023-04-10 19:44:03 +0700

    Test push on new machine

--- Study_Practice/Study_Practice/Main.cs
+++ Study_Practice/Study_Practice/Main.cs
@@ -3,7 +3,7 @@
 namespace Study_Practice;
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         Problem_000 Problem = new Problem_002();
         Problem.Solution();
